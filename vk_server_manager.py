d8c44fa902b661ecd7e5d88b4252e47c9a9e9ca8
mrsigman32 <69324043+mrsigman32@users.noreply.github.com>
2020-08-09 23:19:55 +0300

    Add files via upload

---
+++ vk_server_manager.py
@@ -0,0 +1,40 @@
+import sys
+from threading import Thread
+from config import group_vk_api_token
+from vk_server import VkServer
+
+
+class Server(Thread):
+
+    def __init__(self, vk_server_class):
+        Thread.__init__(self)
+        self.server = vk_server_class
+
+    def run(self):
+        self.server.mainloop()
+
+
+def add_server(server: VkServer, dictionary):
+
+    dictionary[server.get_server_name()] = server
+
+
+def run_all(dictionary):
+    for server in dictionary:
+        if len(sys.argv) > 1:
+            if sys.argv[1] == "test":
+                print(f"{dictionary[server].get_server_name()} -- test mode")
+                dictionary[server].start()
+            else:
+                dictionary[server].mainloop()
+        else:
+            dictionary[server].mainloop()
+
+        print(f"Server {dictionary[server].get_server_name()} started!")
+
+
+servers = {}
+add_server(VkServer("P3112 server", "groups_list/P3112.json", '154573014', '6ea0f22a765091ccc6efeb1c1c3211c1c74c2b3224e22d26fb304485d488eae2c5bd8f86179f0359bae7b'), servers)
+
+
+run_all(servers)
